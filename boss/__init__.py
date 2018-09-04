25970f77fb60b1a30e8631a1649fa36fdd46e676
Josh Forman <josh@yoshrote.com>
2018-09-04 13:05:07 -0400

    Update __init__.py

--- boss/__init__.py
+++ boss/__init__.py
@@ -8,8 +8,9 @@ Things to do:
      - initialize from task data pulled by TaskFinder
      - enumerate the combination of args and kwargs to sent to this task
      - provide a function which takes *args and **kwargs to execute the task
-    ScheduleFinder
-     - querys a (file|db) for the sort of schedules available
+     - specify the type of schedule to be used for this task
+    ScopeFinder
+     - querys a (file|db) for the set of parameter combinations to send to the task
     Scheduler
      - initialize from schedule data pulled by ScheduleFinder
      - determine if a task and arguement combination should run
